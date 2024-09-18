fa39da785860ec5b80b3678fdff961cb8dfab1e3
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2024-09-18 16:12:43 +0700

    add cookies

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tapyn7-fp3-8=g5+d(u*wbr=)pmsqe-%h_dy=b%80qxpbo)$w#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","gabriel-selwas-mentalhealthtracker2.pbp.cs.ui.ac.id","https://pbp.cs.ui.ac.id/web/project/gabriel.selwas/mentalhealthtracker2"]
 
 
