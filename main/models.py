7a538757425c6bb25d8e4da59e92fc40a82f71fb
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2024-10-02 15:43:24 +0700

    add ajax and javasript

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ import uuid
 
     
 class MoodEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
